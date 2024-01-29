ac94ce996db9c74c25e224fb921a3020a8728e1a
mobinbr <mobinbr99@gmail.com>
2024-01-29 16:47:38 +0330

    two functions added for calculating accuracy & testing

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ from nltk.tokenize import word_tokenize
 import sys
 from textblob import TextBlob
 import numpy as np
+import time
 
 def preprocess(tweet_string):
     # clean the data and tokenize it
@@ -23,12 +24,12 @@ def preprocess(tweet_string):
     tweet_string = re.sub(r"(@[^\s]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|http.+", "", tweet_string)
 
     # spell checking
-    tweet_string = TextBlob(tweet_string)
-    checked_string = str(tweet_string.correct())
+    # tweet_string = TextBlob(tweet_string)
+    # checked_string = str(tweet_string.correct())
 
     # removing stopwords
     stop_words = set(stopwords.words('english'))
-    toks = word_tokenize(checked_string)
+    toks = word_tokenize(tweet_string)
     # np.concatenate(stop_words, ['?'], [''])
     filtered_toks = [word for word in toks if word not in stop_words]
 
@@ -41,7 +42,7 @@ def preprocess(tweet_string):
     return features
 
 def load_data(data_path):
-    # load the csv file and return the data
+    # load the train csv file and return the data
     data = []
     with open(data_path, mode ='r') as file:
         csvFile = csv.reader(file)
@@ -51,13 +52,53 @@ def load_data(data_path):
             data.append((tweet, lines[4]))
     return data
 
+def calculate_accuracy(data_path):
+    # load the eval csv file and return accuracy
+    correct_labed = 0
+    total = 0
+    with open(data_path, mode ='r') as file:
+        csvFile = csv.reader(file)
+        next(csvFile)
+        for lines in csvFile:
+            tweet = preprocess(str(lines[2]))
+            c = nb_classifier.classify(tweet)
+            if(c == lines[4]):
+                correct_labed += 1
+            total += 1
+        # print(len(csvFile))
+    # print(total)
+    accuray = correct_labed/total
+    return accuray
+
+def test(data_path, result_path):
+    result = open(result_path, mode='w+')
+    with open(data_path, mode ='r') as file:
+        csvFile = csv.reader(file)
+        next(csvFile)
+        for lines in csvFile:
+            tweet = preprocess(str(lines[2]))
+            c = nb_classifier.classify(tweet)
+            result.write(f"{c}\n")
+    result.close()
 
 # train your model and report the duration time
 train_data_path = "train_data.csv"
 classes = ['positive', 'negative', 'neutral']
 nb_classifier = NaiveBayesClassifier(classes)
+
+start_time = time.time()
 nb_classifier.train(load_data(train_data_path))
+end_time = time.time()
+elapsed_time = end_time - start_time
+print("Model trained successfully!")
+print(f"Train time: {elapsed_time}")
+
+eval_accuracy = calculate_accuracy("data_eval.csv")
+print("Accuracy calculated successfully!")
+print(f"Accuracy: {eval_accuracy}")
+
+test("test_data_nolabel", "result.txt")
+print("Test data labeled successfully!")
 
 test_string = "I love playing football"
-
 print(nb_classifier.classify(preprocess(test_string)))
